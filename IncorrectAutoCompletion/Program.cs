6d8e959d4d554c2c4aa245090e14c2b61c34036b
devhl-labs <shawnkanyer@gmail.com>
2020-12-02 19:55:40 -0500

    init

---
+++ IncorrectAutoCompletion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace IncorrectAutoCompletion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Foo
+    {
+        public string Bar { get; set; }
+    }
+
+    class Fiz : DbContext
+    {
+        public DbSet<Foo> Foos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Foo>(e =>
+            {
+                //typing e.HasIndex(p<space> 
+                //autocompletes to e.HasIndex(prop
+            });
+        }
+    }
+}
